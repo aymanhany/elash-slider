0741237a61a21a01a3bb5cc36a11b312e2cf4faa
Ayman Elash <aymanhanyibrahimkhalel@gmail.com>
2022-01-29 22:17:22 +0200

    Update elashslider.php

--- elashslider.php
+++ elashslider.php
@@ -6,6 +6,8 @@
  * Version:           1.0.0
  * Author:            Ayman Elash
  * Author Uri: facebook.com/wordpressace
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 
